a8cb4c69ff1a4a75e88b46d022d1afb76e73ce8d
stargamer3 <juliengrijalva@gmail.com>
2018-12-15 09:16:28 -0700

    Update index.js

--- bricks/index.js
+++ bricks/index.js
@@ -47,7 +47,7 @@ function draw(){
 						if(g[k][j]>0 && center[0]+2*(i-zeroI+2)*Math.sin(θ)>j*70+1 && center[0]+2*(i-zeroI+2)*Math.sin(θ)<j*70+71&&center[1]-2*(i-zeroI+2)*Math.cos(θ)*θy<i*40+41&&center[1]-2*(i-zeroI+2)*Math.cos(θ)*θy>i*40+1){
 							center = [center[0]+2*(i-zeroI+2)*Math.sin(θ),center[1]-2*(i-zeroI+2)*Math.cos(θ)*θy];
 							zeroI = i;
-							θ = -θ;
+							θy = -θy;
 						}
 					}
 				}
