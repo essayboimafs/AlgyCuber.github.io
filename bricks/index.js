b9bef2d97dca8a2736eaec6b1a867f312f515bf0
AlgyCuber <45890077+AlgyCuber@users.noreply.github.com>
2018-12-16 06:53:15 +0700

    Update index.js

--- bricks/index.js
+++ bricks/index.js
@@ -45,7 +45,7 @@ function draw(){
 				for(let k=0; k<13; k++){
 					for(let j=0; j<6; j++){
 						if(g[k][j]>0 && center[0]+2*(i-zeroI+2)*Math.sin(θ)>j*70+1 && center[0]+2*(i-zeroI+2)*Math.sin(θ)<j*70+71&&center[1]-2*(i-zeroI+2)*Math.cos(θ)*θy<i*40+41&&center[1]-2*(i-zeroI+2)*Math.cos(θ)*θy>i*40+1){
-							if(center[1]-2*(i-zeroI+1)*Math.cos(θ)*θy>(i-1)*40+41){
+							if(center[1]-2*(i-zeroI+1)*Math.cos(θ)*θy>(i-1)*40+41 || center[1]-2*(i-zeroI+1)*Math.cos(θ)*θy>(i-1)*40+1){
 								center = [center[0]+2*(i-zeroI+2)*Math.sin(θ),center[1]-2*(i-zeroI+2)*Math.cos(θ)*θy];
 								zeroI = i;
 								θy = -θy;
