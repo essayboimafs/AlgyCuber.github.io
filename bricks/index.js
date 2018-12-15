0ba26e6fff61dc4ae45cab71dfc5c1359e58ecac
stargamer3 <juliengrijalva@gmail.com>
2018-12-15 09:10:18 -0700

    Update index.js

--- bricks/index.js
+++ bricks/index.js
@@ -1,4 +1,4 @@
-console.log(8);
+console.log(9);
 let bullets = 1;
 let g = [];
 let shooting = false;
@@ -28,7 +28,6 @@ function draw(){
 		else{θ = null;}
 		if(θ!=null){
 			let θy = 1;
-			let bottom = false;
 			let center = [211,509];
 			let zeroI = 0;
 			for(let i=0;i<231;i++){
@@ -45,21 +44,10 @@ function draw(){
 				}
 				for(let k=0; k<13; k++){
 					for(let j=0; j<6; j++){
-						if(g[k][j]>0 && center[0]+2*(i-zeroI+2)*Math.sin(θ)>j*70+1 && center[0]+2*(i-zeroI+2)*Math.sin(θ)<j*70+71){
+						if(g[k][j]>0 && center[0]+2*(i-zeroI+2)*Math.sin(θ)>j*70+1 && center[0]+2*(i-zeroI+2)*Math.sin(θ)<j*70+71&&center[1]-2*(i-zeroI+2)*Math.cos(θ)*θy==i*40+41){
 							center = [center[0]+2*(i-zeroI+2)*Math.sin(θ),center[1]-2*(i-zeroI+2)*Math.cos(θ)*θy];
 							zeroI = i;
-							if(!bottom){
-							θ = -θ;
-							}
-							else{
-							//θy *= -1;
-							}
-						}
-						else if(g[k][j]>0 && center[1]-2*(i-zeroI+2)*Math.cos(θ)*θy>k*40+1 && center[1]-2*(i-zeroI+2)*Math.cos(θ)*θy<k*40+41){
-							bottom = true;
-						}
-						else{
-						bottom = false;
+							θy = -θy;
 						}
 					}
 				}
