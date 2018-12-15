7082065f769aa99973a4931b7e7abc42810898df
AlgyCuber <45890077+AlgyCuber@users.noreply.github.com>
2018-12-15 13:14:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//02
+//03
 let bullets = 1;
 let g = [];
 let shooting = false;
@@ -58,7 +58,7 @@ function draw(){
 				fill(152,244,66);
 				rect(j*70+1,i*40+1,70,40);
 				fill(0);
-				text(g[i][j],j*70+36,i*40+30);
+				text(g[i][j],j*70+36,i*40+25);
 			}
 			else if(g[i][j]<0){
 				fill(220,232,242);
