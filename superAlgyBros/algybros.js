c347b2e0fedc23bb64ebc0f46efe1ea659284c3b
AlgyCuber <45890077+AlgyCuber@users.noreply.github.com>
2018-12-16 07:42:03 +0700

    Update algybros.js

--- superAlgyBros/algybros.js
+++ superAlgyBros/algybros.js
@@ -1,4 +1,14 @@
-console.log(7);
+console.log(8);
+let lvls = [{
+  world:1,
+  lvl:1,
+  coincash:false,
+  displayName:'1-1',
+  theme:0, //0 = overworld, 1 = underground, 2 = island, 3 = castle, 4 = sky, 5 = underwater
+  night:false,
+  snow:false
+}];
+let currentLvl = 0;
 function character(){
   this.draw1 = function(){
   ellipse(20,20,20,20);
@@ -10,5 +20,10 @@ function setup(){
   ch = new character();
 }
 function draw(){
+  if(lvls[currentLvl].theme==0){background(93,148,251);}
+  else if(lvls[currentLvl].theme==1){}
+  else if(lvls[currentLvl].theme==2){}
+  else if(lvls[currentLvl].theme==3){}
+  else if(lvls[currentLvl].theme==4){background(219,252,255);}
   ch.draw1();
 }
