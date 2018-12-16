657f999704d18baee1d953bc7afcdcff879546ab
stargamer3 <juliengrijalva@gmail.com>
2018-12-15 17:24:49 -0700

    Update algybros.js

--- superAlgyBros/algybros.js
+++ superAlgyBros/algybros.js
@@ -1,11 +1,11 @@
-console.log(5);
+console.log(6);
 function character(){
   this.draw1 = function(){
   ellipse(20,20,20,20);
   }
 }
 let ch;
-function setup{
+function setup(){
   ch = new character();
 }
 function draw(){
