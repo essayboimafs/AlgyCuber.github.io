3fc8a6aeec99376a56a2819da37008ddbf2b36b5
stargamer3 <juliengrijalva@gmail.com>
2018-12-15 17:54:22 -0700

    Update algybros.js

--- superAlgyBros/algybros.js
+++ superAlgyBros/algybros.js
@@ -6,12 +6,37 @@ let lvls = [{
   displayName:'1-1',
   theme:0, //0 = overworld, 1 = underground, 2 = island, 3 = castle, 4 = sky, 5 = underwater
   night:false,
-  snow:false
+  snow:false,
+  layout:[[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]] //0 - empty, 1 - character, 2 - ground, 3 - question block, 4 - bricks, 5 - pipetop, 6 - pipeshaft, 7 - invisiquestions, 8 - invisabricks, 9 - coins, 10 - oompa loompa
 }];
 let currentLvl = 0;
-function character(){
+function character(ox, oy){
+  this.x = ox;
+  this.y = oy;
   this.draw1 = function(){
-  ellipse(20,20,20,20);
+  ellipse(this.x,this.y,20,20);
+  }
+}
+function enemy(ox, oy){
+  this.x = ox;
+  this.y = oy;
+  this.draw1 = function(){
+  ellipse(this.x,this.y,20,20);
   }
 }
 let ch;
