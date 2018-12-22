b7a256d31cb9536bc8db9f1f384a37920e875d3f
AlgyCuber <45890077+AlgyCuber@users.noreply.github.com>
2018-12-22 11:09:34 +0700

    Update tttAI.js

--- tttAI/tttAI.js
+++ tttAI/tttAI.js
@@ -1,3 +1,12 @@
-console.log(0);
-function setup(){}
-function draw(){}
+console.log(1);
+let g = [0,0,0,0,0,0,0,0,0];
+function setup(){
+  createCanvas(194,194);
+}
+function draw(){
+  for(let i=0;i<3;i++){
+    for(let j=0;j<3;j++){
+      rect(i*64+1,j*64,1);
+    }
+  }
+}
