5acecd2e9bba1b19017a4e7bba513be63b6d18c0
AlgyCuber <45890077+AlgyCuber@users.noreply.github.com>
2018-12-22 14:38:36 +0700

    Update tttAI.js

--- tttAI/tttAI.js
+++ tttAI/tttAI.js
@@ -1,6 +1,7 @@
-console.log(7);
+console.log(8);
 let g = [0,0,0,0,0,0,0,0,0];
 let turn = 1;
+let over = false;
 function setup(){
   createCanvas(300,300);
 }
@@ -17,7 +18,17 @@ function draw(){
     if(g[i]>0){text('X',i%3*100+50,Math.floor(i/3)*100+65);}
     else if(g[i]<0){text('O',i%3*100+50,Math.floor(i/3)*100+65);}
   }
-  if(turn<0){
+  //win detection
+  if((g[0]>0 && g[1]>0 && g[2]>0) || (g[3]>0 && g[4]>0 && g[5]>0) || (g[6]>0 && g[7]>0 && g[8]>0) || (g[0]>0 && g[3]>0 && g[6]>0) || (g[1]>0 && g[4]>0 && g[7]>0) || (g[2]>0 && g[5]>0 && g[8]>0) || (g[0]>0 && g[4]>0 && g[8]>0) || (g[2]>0 && g[4]>0 && g[6]>0)){
+    alert('X Wins');
+    over = true;
+  }
+  if((g[0]<0 && g[1]<0 && g[2]<0) || (g[3]<0 && g[4]<0 && g[5]<0) || (g[6]<0 && g[7]<0 && g[8]<0) || (g[0]<0 && g[3]<0 && g[6]<0) || (g[1]<0 && g[4]<0 && g[7]<0) || (g[2]<0 && g[5]<0 && g[8]<0) || (g[0]<0 && g[4]<0 && g[8]<0) || (g[2]<0 && g[4]<0 && g[6]<0)){
+    alert('O Wins');
+    over = true;
+  }
+  //computer
+  if(turn<0 && !over){
     let a = Math.floor(Math.random()*9);
     if(g[a]==0){move(a);}
   }
@@ -27,7 +38,7 @@ function move(sq){
   turn *= -1;
 }
 function mousePressed(){
-  if(turn>0){
+  if(turn>0 && !over){
     let sqX,sqY;
     if(mouseX>0 && mouseX<100){sqX = 0;}
     else if(mouseX>100 && mouseX<200){sqX = 1;}
@@ -35,6 +46,6 @@ function mousePressed(){
     if(mouseY>0 && mouseY<100){sqY = 0;}
     else if(mouseY>100 && mouseY<200){sqY = 1;}
     else if(mouseY>200 && mouseY<300){sqY = 2;}
-    move(sqY*3+sqX);
+    if(g[sqY*3+sqX]!=0){move(sqY*3+sqX);}
   }
 }
