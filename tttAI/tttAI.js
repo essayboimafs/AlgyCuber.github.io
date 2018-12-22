37e0ed1fd5ef3f3eb1930e44206a178c110eba2c
AlgyCuber <45890077+AlgyCuber@users.noreply.github.com>
2018-12-22 11:21:53 +0700

    Update tttAI.js

--- tttAI/tttAI.js
+++ tttAI/tttAI.js
@@ -1,12 +1,12 @@
-console.log(3);
+console.log(4);
 let g = [0,0,0,0,0,0,0,0,0];
 function setup(){
-  createCanvas(194,194);
+  createCanvas(300,300);
 }
 function draw(){
-  for(let i=0;i<3;i++){
-    for(let j=0;j<3;j++){
-      rect(1,1,i*64+1,j*64+1);
-    }
-  }
+  strokeWeight(3);
+  line(0,100,300,100);
+  line(0,200,300,200);
+  line(100,0,100,300);
+  line(200,0,200,300);
 }
