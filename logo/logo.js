25f9cd51a209a19be873f3dd843d952b8f1df7d0
AlgyCuber <45890077+AlgyCuber@users.noreply.github.com>
2019-01-25 09:21:06 +0700

    Update logo.js

--- logo/logo.js
+++ logo/logo.js
@@ -1,15 +1,15 @@
-console.log(3);
+console.log(4);
 let input;
 let sc = [255,255,255];
 let pc = [255,255,255];
 let fc = [255,255,255];
 function setup(){
-  createCanvas(1200,1000);
+  createCanvas(800,600);
 }
 function draw(){
   background(255);
   stroke(0);
   strokeWeight(1);
   fill(sc[0],sc[1],sc[2]);
-  rect(0,0,1000,1000);
+  rect(0,0,600,600);
 }
