7d21348297eea7a3347e996321093a0b9af242f1
AlgyCuber <45890077+AlgyCuber@users.noreply.github.com>
2019-01-25 08:58:52 +0700

    Update logo.js

--- logo/logo.js
+++ logo/logo.js
@@ -1,1 +1,10 @@
-console.log(0);
+console.log(1);
+let input;
+function setup(){
+  createCanvas(1000,1000);
+  input = createInput();
+  input.position(1050,50);
+}
+function draw(){
+  background(255);
+}
