44623b14db709de3e80b85a88d4e18df1d99c402
AlgyCuber <45890077+AlgyCuber@users.noreply.github.com>
2019-01-26 16:13:42 +0700

    Update logo.js

--- logo/logo.js
+++ logo/logo.js
@@ -1,15 +1,10 @@
-console.log(12);
+console.log(13);
 let sc = [255,255,255];
 let pc = [255,255,255];
 let fc = [255,255,255];
 let cmd = '';
 function setup(){
   createCanvas(601,601);
-  document.body.innerHTML += '<input id="inp">';
-  /*inp.style.position = 'absolute';
-  inp.style.left = '650px';
-  inp.style.top = '100px';
-  inp.width = 400;*/
 }
 function draw(){
   background(255);
